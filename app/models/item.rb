4b02cd801c319f3ddfe5d8ef3405516eb60730ee
hizaoni <hizaoni@yahoo.co.jp>
2022-05-19 18:50:19 +0900

    デザイン修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,5 @@ class Item < ApplicationRecord
     validates :quantity
     validates :order_point
   end
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
 end
