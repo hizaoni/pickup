47c618465bf7e9d458131ff276b2f40505105b9d
hizaoni <hizaoni@yahoo.co.jp>
2022-05-20 22:04:53 +0900

    カテゴリー絞り込みjavaScript実装

--- app/javascript/stock.js
+++ app/javascript/stock.js
@@ -10,14 +10,13 @@ function stockChoice(){
     XHR.onload = () => {
       const label = document.getElementById('stock-form-label');
       const itemSelect = document.getElementById('stock-item-select');
-      itemSelect.remove()
+      itemSelect.innerHTML = "";
       const items = XHR.response.selected_category_items;
-      const html = `
-      <select class="stock_form_content" id="stock-item-select" name="stock[item_id]">
-      <option value="${item.id}">${item.name}</option>
-      </select>`;
-      
-      label.insertAdjacentHTML("afterend", html);
+      items.forEach(function( item ) {
+        const html = `
+      <option value="${item.id}">${item.name}</option>`;
+      itemSelect.insertAdjacentHTML("afterbegin", html);
+      });
       
     };
 
