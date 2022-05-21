fbefa95778eb056e2355bc76468641887c7a1f27
hizaoni <hizaoni@yahoo.co.jp>
2022-05-21 17:20:24 +0900

    在庫数表示

--- app/javascript/stock.js
+++ app/javascript/stock.js
@@ -1,4 +1,5 @@
 function stockChoice(){
+  const currentItemQuantity = document.getElementById("current-item-quantity");
   const category = document.getElementById("stock-category-select");
   const changeCategory = category.addEventListener("change", () => {
     const stockForm = document.getElementById("stock-form");
@@ -11,26 +12,52 @@ function stockChoice(){
       const label = document.getElementById('stock-form-label');
       const itemSelect = document.getElementById('stock-item-select');
       itemSelect.innerHTML = "";
+      currentItemQuantity.innerHTML = "";
       const items = XHR.response.selected_category_items;
-      console.log(items);
+      const quantity = XHR.response.quantity;
       if (items.length == 0 ) {
         const html = `
           <option value="">なし</option>`;
           itemSelect.insertAdjacentHTML("afterbegin", html);
-      } else {
-        items.forEach(function( item ) {
+      } else { 
+        if ( items.length == 1 ) {
+          items.forEach(function( item ) {
+            const html1 = `
+            <option value="${item.id}">${item.name}</option>`;
+            itemSelect.insertAdjacentHTML("afterbegin", html1);
+          });
+          const html2 = `
+          在庫数　${quantity.quantity} ${items[0].unit}`;
+          currentItemQuantity.insertAdjacentHTML("afterbegin", html2);
+          
+        } else {
+          items.forEach(function( item ) {
           const html = `
           <option value="${item.id}">${item.name}</option>`;
           itemSelect.insertAdjacentHTML("afterbegin", html);
-        });
-
-      };
-     
-    };
-
-
+          })
+        }
+      }     
+    }
   });
-
+  const item = document.getElementById("stock-item-select");
+  const changeItem = item.addEventListener("change", () => {
+    const stockForm = document.getElementById("stock-form");
+    let formData = new FormData(stockForm);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", '/items/quantity', true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      const currentItemQuantity = document.getElementById("current-item-quantity");
+      const item = XHR.response.quantity;
+      const unitItem = XHR.response.select_item
+      currentItemQuantity.innerHTML = "";
+      const html = `
+        在庫数　${item.quantity} ${unitItem.unit}`;
+      currentItemQuantity.insertAdjacentHTML("afterbegin", html);
+    };
+  })
 
 };
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources 'items'
   delete 'items', to: 'items#destroy_many'
   post 'items/select', to: 'items#category_select'
+  post 'items/quantity', to: 'items#item_quantity'
   resources 'stocks', only: [:new, :create]
   resources 'ships', only: [:new, :create, :index]
 end
