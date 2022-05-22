6440ccb6847ecbc7c6cd9b52f453aba43bce49c5
hizaoni <hizaoni@yahoo.co.jp>
2022-05-22 09:13:42 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :item_find, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order(id: "DESC")
+    @items = Item.all.order(id: 'DESC')
   end
 
   def new
@@ -57,45 +57,43 @@ class ItemsController < ApplicationController
   end
 
   def category_select
-    if category_param[:category_id] == "1"
+    if category_param[:category_id] == '1'
       selected_category_items = Item.all
-      render json: {selected_category_items: selected_category_items}
-      return
+      render json: { selected_category_items: selected_category_items }
+      nil
     else
       selected_category_items = Item.where(category_id: category_param[:category_id])
       if selected_category_items.length == 1
-        select_item_quantity = selected_category_items[0].quantity + Stock.where(item_id: select_item_param[:item_id]).sum(:quantity) -  Ship.where(item_id: select_item_param[:item_id]).sum(:quantity)
-        quantity = Hash.new
-        quantity.store("quantity", select_item_quantity)
-        render json: {selected_category_items: selected_category_items, quantity: quantity}
-        return
+        select_item_quantity = selected_category_items[0].quantity + Stock.where(item_id: select_item_param[:item_id]).sum(:quantity) - Ship.where(item_id: select_item_param[:item_id]).sum(:quantity)
+        quantity = {}
+        quantity.store('quantity', select_item_quantity)
+        render json: { selected_category_items: selected_category_items, quantity: quantity }
+        nil
       else
-        render json: {selected_category_items: selected_category_items}
+        render json: { selected_category_items: selected_category_items }
       end
     end
   end
 
   def item_quantity
     select_item = Item.find_by(id: select_item_param[:item_id])
-    select_item_quantity = select_item.quantity + Stock.where(item_id: select_item_param[:item_id]).sum(:quantity) -  Ship.where(item_id: select_item_param[:item_id]).sum(:quantity)
-    quantity = Hash.new
-    quantity.store("quantity", select_item_quantity)
-    render json: {select_item: select_item, quantity: quantity}
-    
+    select_item_quantity = select_item.quantity + Stock.where(item_id: select_item_param[:item_id]).sum(:quantity) - Ship.where(item_id: select_item_param[:item_id]).sum(:quantity)
+    quantity = {}
+    quantity.store('quantity', select_item_quantity)
+    render json: { select_item: select_item, quantity: quantity }
   end
 
   def search_item
     search_category_items = Item.where(category_id: params[:category_id])
     category = Category.find(params[:category_id]).name
-    
+
     quantity = []
     search_category_items.each do |item|
-      select_item_quantity = item.quantity + Stock.where(item_id: item.id).sum(:quantity) -  Ship.where(item_id: item.id).sum(:quantity)
+      select_item_quantity = item.quantity + Stock.where(item_id: item.id).sum(:quantity) - Ship.where(item_id: item.id).sum(:quantity)
       quantity.push(select_item_quantity)
     end
-    render json: {search_category_items: search_category_items, quantity: quantity, category: category}
+    render json: { search_category_items: search_category_items, quantity: quantity, category: category }
   end
-
 
   private
 
@@ -111,17 +109,17 @@ class ItemsController < ApplicationController
   end
 
   def category_param
-    if params.key?("stock")
+    if params.key?('stock')
       params.require(:stock).permit(:category_id)
-    elsif params.key?("ship")
+    elsif params.key?('ship')
       params.require(:ship).permit(:category_id)
     end
   end
 
   def select_item_param
-    if params.key?("stock")
+    if params.key?('stock')
       params.require(:stock).permit(:item_id)
-    elsif params.key?("ship")
+    elsif params.key?('ship')
       params.require(:ship).permit(:item_id)
     end
   end
