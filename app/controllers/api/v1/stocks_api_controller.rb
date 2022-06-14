366b9df68263412887e4d509d4fba80f2b3e0c61
hizaoni <hizaoni@yahoo.co.jp>
2022-06-14 16:56:10 +0900

    stocks_api実装

--- app/controllers/api/v1/stocks_api_controller.rb
+++ app/controllers/api/v1/stocks_api_controller.rb
@@ -1,25 +1,22 @@
 module Api
   module V1
     class StocksApiController < ApplicationController
+      skip_before_action :verify_authenticity_token
       def new
         @items = Item.all
         @stock = Stock.new
       end
 
       def create
-        @stock = Stock.new(stock_param)
-        if @stock.save
-          redirect_to root_path
-        else
-          render :new
-        end
+        Stock.create(stock_param)
+
       end
 
 
       private
 
       def stock_param
-        params.require(:stock).permit(:quantity, :item_id)
+        params.require(:stocks_api).permit(:quantity, :item_id)
       end
     end
   end
